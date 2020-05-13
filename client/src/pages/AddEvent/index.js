e3411f763f5344c58c552c8b0f896c3b5be5d7c5
shanik hernandez <shaniik.her93@gmail.com>
2020-05-13 11:34:47 -0400

    added authentication to add event page

---
+++ client/src/pages/AddEvent/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Card from "./Card"
+
+function AddEvent (){
+    const [user, dispatch] = useContext(UserContext)
+	console.log(user)
+
+	useEffect(() => {
+		fetch('api/users/user', {
+			credentials: 'include'
+		})
+			.then((res) => {
+				console.log(`response to authenticate ${res}`);
+				return res.json(res)
+
+			})
+			.then(data => {
+				console.log(data);
+				dispatch({
+					type: "GET_USER",
+					payload: data
+				})
+
+			})
+			.catch((err) => {
+				console.log('Error fetching authorized user.');
+			});
+
+    }, []);
+    
+    // return (
+
+    // )
+}
+
+export default AddEvent;
