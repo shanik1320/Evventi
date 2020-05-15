579de03c42c14a63b35989d868c2b3c904705d82
shanik hernandez <shaniik.her93@gmail.com>
2020-05-15 11:06:40 -0400

    changes

--- client/src/pages/MyEvents/index.js
+++ client/src/pages/MyEvents/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import EventForm from "../../components/EventForm";
+import Card from "../../components/Card"
 
 function MyEvents () {
     return (
-        <EventForm/>
+        <div>
+            <Card>
+
+            </Card>
+            <Redirect from="/addevent" to="/myevents"/>
+        </div>
     )
 }
 export default MyEvents;
