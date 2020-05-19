2d11b90ea4552bb89aaaa66c6d4d0cf891c712a4
shanik hernandez <shaniik.her93@gmail.com>
2020-05-19 13:33:12 -0400

    made all events page

--- client/src/App.js
+++ client/src/App.js
@@ -53,7 +53,7 @@ const AuthExample = () => (
 					<Switch>
 						<Route path="/public" component={PublicRoute} />
 						<Route path="/login" component={Login} />
-						<Route path="/Profile" component={Profile} />
+						<PrivateRoute path="/Profile" component={Profile} />
 						<Route path="/register" component={Register} />
 						<PrivateRoute path="/protected" component={ProtectedRoute} />
 						<PrivateRoute path="/addevent" component={AddEvent}/>
---
+++ client/src/pages/Events/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import AllEvents from "../../components/AllEvents"
+
+
+function Events() {
+
+    return (
+        <div className="container">
+            <AllEvents />
+            {/* <Redirect to={{
+                             pathname: '/myprofile',
+                         }} /> */}
+
+
+        </div>
+    )
+
+}
+export default Events;
